f37b50358d8fb064d3b76d74d03ae1db8b6187c9
Shakhzodkhon Jalilov <70517130+shakhzodkhonjalilov@users.noreply.github.com>
2023-03-30 12:07:00 -0500

    test2

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
-function test() {
+function test (value) {
+  console.log('test');
+  if (value === 'test') {
     console.log('test');
+  }
+
+  if (value === undefined) {
+    console.log('test');
+  }
+
+  if (!(value instanceof Date)) console.log('sagsdadf');
 }
+
+test('test');
